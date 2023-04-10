4be0c05baf5a630662530e996f6b001f79edcad7
saifulaija <saifulaija2023@gmail.com>
2023-04-10 20:10:05 +0600

    job details display added

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -25,9 +25,21 @@ const JobDetails = () => {
 
 
     return (
-        <div>
+        <div className='m-20'>
            <div>
            <h1 className="text-center text-4xl font-semibold" >Job Details</h1>
+           </div>
+
+           <div>
+            <div>
+                <h4 className='text-xl font-bold'>Job Description : <small className='font-normal'>{result[0].jobDescription}</small></h4>
+                <h4 className='text-xl font-bold my-6'>Job Resposiblities : <small className='font-normal'> {result[0].jobResponsibility}</small> </h4>
+                <h4 className='text-xl font-bold'>Educational Requirement : <small className='font-normal'> {result[0].educationRequirements}</small> </h4>
+                <h4 className='text-xl font-bold mb-6'>Experience : <small className='font-normal'>{result[0].experience}</small> </h4>
+            </div>
+            <div>
+
+            </div>
            </div>
         </div>
     );
